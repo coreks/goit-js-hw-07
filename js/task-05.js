87399f8aeaba8cb709e7f8621ee2a5125ae9ef8c
coreks <aleksobchenko@gmail.com>
2021-06-06 16:15:19 +0300

    task-05 ok

--- js/task-05.js
+++ js/task-05.js
@@ -2,10 +2,12 @@ const inputNameEl = document.querySelector('#name-input');
 const outputNameEl = document.querySelector('#name-output');
 
 inputNameEl.addEventListener('input', onInputChange);
-
+let text = [];
 function onInputChange(event) {
   outputNameEl.textContent = event.currentTarget.value;
-  if (!inputNameEl.textContent) {
-    return outputNameEl.textContent;
+  if (!event.currentTarget.value) {
+    outputNameEl.textContent = 'незнакомец';
   }
 }
+// console.log(inputTextEl);
+// console.log(outputNameEl.textContent);
